de123c364c859019303a1b2fba215fc49f72db61
Yassine Ouicha Hammou <yassineouichahammou@gmail.com>
2025-01-30 12:24:01 +0100

    Testing the 5% discount foncionality, all the integration tests passed

--- src/test/java/com/parkit/parkingsystem/integration/ParkingDataBaseIT.java
+++ src/test/java/com/parkit/parkingsystem/integration/ParkingDataBaseIT.java
@@ -1,5 +1,6 @@
 package com.parkit.parkingsystem.integration;
 
+import com.parkit.parkingsystem.constants.Fare;
 import com.parkit.parkingsystem.constants.ParkingType;
 import com.parkit.parkingsystem.dao.ParkingSpotDAO;
 import com.parkit.parkingsystem.dao.TicketDAO;
@@ -17,6 +18,8 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Date;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
@@ -78,10 +81,6 @@ public class ParkingDataBaseIT {
         // We simulate the vehicle entry to the parking
         testParkingACar();
 
-        // We ensure a ticket is generated after entry
-        Ticket ticketBeforeExit = ticketDAO.getTicket("ABCDEF");
-        assertNotNull(ticketBeforeExit);
-
         // We initialise the parking service
         ParkingService parkingService = new ParkingService(inputReaderUtil, parkingSpotDAO, ticketDAO);
 
@@ -97,5 +96,52 @@ public class ParkingDataBaseIT {
 
         // We ensure a fare is generated
         assertTrue(ticketAfterExit.getPrice() >= 0, "The fare should be generated");
+    }
+
+    @Test
+    public void testParkingLotExitRecurringUser(){
+
+        // Initializing of the parking service
+        ParkingService parkingService = new ParkingService(inputReaderUtil, parkingSpotDAO, ticketDAO);
+
+        // First entry and exit to register the user as a recurring customer
+        parkingService.processIncomingVehicle();
+        parkingService.processExitingVehicle();
+
+        // We verify the user is now recurring
+        int nbTickets = ticketDAO.getNbTicket("ABCDEF");
+        assertTrue(nbTickets > 0, "User should be recognized as recurring");
+
+        // Second entry
+        parkingService.processIncomingVehicle();
+
+        // Update of the entry time to simulate 3 hours of parking
+        Ticket ticketBeforeExit = ticketDAO.getTicket("ABCDEF");
+        assertNotNull(ticketBeforeExit, "Ticket should exist after entry");
+        ticketBeforeExit.setInTime(new Date(System.currentTimeMillis() - (3 * 60 * 60 * 1000))); // 3 hours ago
+
+        // We make sure the inTime is updated, very important
+        ticketDAO.updateTicket(ticketBeforeExit);
+        ticketDAO.saveTicket(ticketBeforeExit);
+
+        // Process vehicle exit
+        parkingService.processExitingVehicle();
+
+        // Retrieve the ticket and verify the fare
+        Ticket ticketAfterExit = ticketDAO.getTicket("ABCDEF");
+        assertNotNull(ticketAfterExit, "The ticket should exist after exit");
+        assertNotNull(ticketAfterExit.getOutTime(), "Out time should be set after exit");
+
+        // Checking parking time
+        System.out.println("CheckIn time is : " + ticketAfterExit.getInTime());
+        System.out.println("-----------------------------------------------------");
+        System.out.println("CheckOut time is : " + ticketAfterExit.getOutTime());
+
+        // We ensure a fare is generated
+        assertTrue(ticketAfterExit.getPrice() > 0, "Fare should be greater than 0");
+
+        // We verify that the 5% discount is applied
+        double expectedPrice = 3 * Fare.CAR_RATE_PER_HOUR * 0.95;
+        assertEquals(expectedPrice, ticketAfterExit.getPrice(), 0.01, "Fare should be reduced by 5% for a recurring user");
     }
 }
